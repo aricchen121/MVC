397f8cf90b3421217ea94c5462b40c814a4ca9b1
Aric Chen <achen1917@gmail.com>
2017-06-27 14:06:20 -0400

    commit 1 add project

---
+++ Simple/src/Calculator/Calculator.java
@@ -0,0 +1,11 @@
+package Calculator;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Apple");
+		System.out.println("Mango");
+	}
+
+}
